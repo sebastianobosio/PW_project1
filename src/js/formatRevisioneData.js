0e57fa6c6e4781f31833e0ca0645f3c8266b6ffc
sebastianobosio <sebastiano.bosio01@gmail.com>
2024-05-08 18:43:54 +0200

    implemented edit function on all pages

--- src/js/formatRevisioneData.js
+++ src/js/formatRevisioneData.js
@@ -1,14 +1,27 @@
 function formatRevisioneData(data) {
     $('#searchResults').empty();
 
-    data.forEach(revisione => {
+    data.forEach(async revisione => {
         const revisioneDiv = $('<div>').addClass('revisione');
         $('<div>').html('Revisione: <span class="numero">' + revisione.numero + '</span>').appendTo(revisioneDiv);
-        $('<div>').html('Data della revisione: <span class="dataRev">' + revisione.dataRev + '</span>').appendTo(revisioneDiv);
+        //$('<div>').html('Data della revisione: <span class="dataRev">' + revisione.dataRev + '</span>').appendTo(revisioneDiv);
+        $('<div>').html('Data della revisione: <span class="dataRev">' + '<input type="date" value="' + revisione.dataRev + '" disabled></input>' + '</span>').appendTo(revisioneDiv);
         $('<div>').html('Targa associata: <span class="targa">' + revisione.targa + '</span>').appendTo(revisioneDiv);
-        $('<div>').html('Esito: <span class="esito">' + revisione.esito + '</span>').appendTo(revisioneDiv);
+        //$('<div>').html('Esito: <span class="esito">' + revisione.esito + '</span>').appendTo(revisioneDiv);
+        const esitoSelect = $('<select>').addClass('esito').prop('disabled', true); // Disable select element initially
+        $('<option>').val('positivo').text('Positivo').appendTo(esitoSelect);
+        $('<option>').val('negativo').text('Negativo').appendTo(esitoSelect);
+        if (revisione.esito === 'positivo') {
+            esitoSelect.val('positivo');
+        } else if (revisione.esito === 'negativo') {
+            esitoSelect.val('negativo');
+        }
+        console.log("porco dio");
+        $('<div>').html('Esito: ').append(esitoSelect).appendTo(revisioneDiv);
+        $('<div>').addClass('motivazioneDiv').css('display', 'none').html('Motivazione: <span class="motivazione"><input type="text" required disabled></input></span>').appendTo(revisioneDiv);
         if (revisione.esito == 'negativo') {
-            $('<div>').html('Motivazione: <span class="motivazione">' + revisione.motivazione + '</span>').appendTo(revisioneDiv);
+            revisioneDiv.find('.motivazioneDiv').toggle(revisione.esito === 'negativo');
+            revisioneDiv.find('.motivazione input').val(revisione.motivazione);
         }
         //info buttons
         const informationBtnDiv = $('<div>').addClass('infoBtn');
@@ -22,12 +35,12 @@ function formatRevisioneData(data) {
 
         //Edit and remove buttons
         const editAndRemoveBtnDiv = $('<div>').addClass('edirmBtn');
-        const editButton = $('<button>').text('Edit').addClass('edit-button');
+        const editButton = await createEditButton(revisioneDiv);
         const removeButton = $('<button>').text('Remove').addClass('remove-button');
         editButton.appendTo(editAndRemoveBtnDiv)
         removeButton.appendTo(editAndRemoveBtnDiv);
-        editButton.on('click', editBtnClicked(revisioneDiv));
-        removeButton.on('click', deleteBtnClicked);
+        //editButton.on('click', editBtnClicked(revisioneDiv));
+        removeButton.on('click', function() {deleteBtnClicked(revisioneDiv)});
         editAndRemoveBtnDiv.appendTo(revisioneDiv);
         revisioneDiv.appendTo($('#searchResults'));
     });
@@ -95,8 +108,8 @@ function targaDaRevisioneBtnClicked(revisione) {
     console.log("bho");
 }
 
-function deleteBtnClicked() {
-    var id = $(this).closest('li').data('id');
+function deleteBtnClicked(revisioneDiv) {
+    var id = revisioneDiv.find('.numero').text();
     var confirmed = confirm("Are you sure you want to delete this entry?");
     if (confirmed) {
         handleAjaxRequest(
@@ -113,19 +126,164 @@ function deleteBtnClicked() {
     }
 }
 
-function editBtnClicked(revisioneDiv) {
-    var listItem = revisioneDiv;
-    var id = listItem.find('.numero');
-    var data = listItem.find('.dataRev');
-    var targa = listItem.find('.targa');
-    var esito = listItem.find('.esito');
-    var motivazione = esito === 'negativo' ? listItem.find('.motivazione') : '';
-    $('#editId').val(id);
-    $('#editDataRev').val(data);
-    $('#editTarga').val(targa);
-    $('#editEsito').val(esito);
-    $('#editMotivazione').val(motivazione);
-    $('#editMotivazioneDiv').toggle(esito === 'negativo');
-    $('.addForm').hide();
-    $('.editForm').show();            
+function editEsitoChanged(revisioneDiv) {
+    console.log("Esito ha cambiato stato: " + revisioneDiv.find('select.esito').val() == 'negativo');
+    revisioneDiv.find('.motivazioneDiv').toggle(revisioneDiv.find('select.esito').val() == 'negativo');
+    revisioneDiv.find('.motivazione input').prop('required', revisioneDiv.find('select.esito').val() == 'negativo');
+    revisioneDiv.find('.motivazione input').prop('disabled', revisioneDiv.find('select.esito').val() == 'positivo');
+    revisioneDiv.find('.motivazione input').prop('disabled', revisioneDiv.find('select.esito').val() == 'positivo');
+}
+
+async function createEditButton(revisioneDiv) {
+    const editButton = $('<button>').text('Edit Fields');
+    
+    async function attachEditHandler() {
+        const id = revisioneDiv.find('.numero').text();
+        var OriginaldataRev = revisioneDiv.find('.dataRev input').prop('disabled', true).val();
+        var Originaltarga = revisioneDiv.find('.targa').attr('contenteditable', false).text();
+        var Originalesito = revisioneDiv.find('select.esito').prop('disabled', true).val();
+        var Originalmotivazione = revisioneDiv.find('.motivazione input').prop('disabled', true).val();
+        console.log("valori prima di cliccare l'edit: " + id + " " + OriginaldataRev + " " + Originaltarga + " " + Originalesito + " " + Originalmotivazione);
+        editButton.off('click').on('click', async function() {
+            revisioneDiv.find('.dataRev input').prop('disabled', false);
+            revisioneDiv.find('.targa').attr('contenteditable', true);
+            var esito = revisioneDiv.find('select.esito').prop('disabled', false).val();
+            if (esito === 'negativo') {
+                revisioneDiv.find('.motivazione input').prop('disabled', false);
+            }
+    
+            revisioneDiv.find('select.esito').change(function() { editEsitoChanged(revisioneDiv); });
+    
+            editButton.text('Save Changes').off('click').on('click', async function() {
+                const id = revisioneDiv.find('.numero').text();
+                var dataRevObj = revisioneDiv.find('.dataRev input').prop('disabled', true)
+                var targaObj = revisioneDiv.find('.targa').attr('contenteditable', false)
+                var esitoObj = revisioneDiv.find('select.esito').prop('disabled', true)
+                var motivazioneObj = revisioneDiv.find('.motivazione input').prop('disabled', true);
+                var motivazione = motivazioneObj.val();
+                var esito = esitoObj.val();                
+                if (esito === 'negativo' && motivazione === "") {
+                    alert("La motivazione non può essere nulla in caso di esito negativo");
+                    motivazioneObj.prop('disabled', false); // Re-enable input
+                    dataRevObj.prop('disabled', false);
+                    targaObj.attr('contenteditable', true);
+                    esitoObj.prop('disabled', false);
+                    motivazioneObj.text(Originalmotivazione);
+                    dataRevObj.val(OriginaldataRev);
+                    targaObj.text(Originaltarga);
+                    esitoObj.val(Originalesito);
+                    if (Originalesito == 'positivo') {
+                        motivazioneObj.prop('disabled', true)
+                        revisioneDiv.find('.motivazioneDiv').toggle(revisioneDiv.find('select.esito').val() == 'negativo');
+                    }
+                    return false; // Prevent form submission
+                }
+              
+                var targa = targaObj.text();
+                var dataRev = dataRevObj.val();
+
+                const dataUpdateRequest = "editId=" + id + "&editDataRev=" + dataRev + "&editTarga=" + targa + "&editEsito=" + esito + "&editMotivazione=" + motivazione + "&action=update";  
+                
+                if (await checkRevision(targa, dataRev)) {
+                    try {
+                        await saveChanges(dataUpdateRequest);
+                        console.log("sono qui")
+                        editButton.text('Edit Fields');
+                        attachEditHandler();
+                    } catch (error) {
+                        console.log("non funziona un cazzo");
+                    }
+                    
+                    console.log("aggiornare stato bottone");
+                } else {
+                    motivazioneObj.prop('disabled', false); // Re-enable input
+                    dataRevObj.prop('disabled', false);
+                    targaObj.attr('contenteditable', true);
+                    esitoObj.prop('disabled', false);
+                    motivazioneObj.text(Originalmotivazione);
+                    dataRevObj.val(OriginaldataRev);
+                    targaObj.text(Originaltarga);
+                    esitoObj.val(Originalesito);
+                    if (Originalesito == 'positivo') {
+                        motivazioneObj.prop('disabled', true)
+                        revisioneDiv.find('.motivazioneDiv').toggle(revisioneDiv.find('select.esito').val() == 'negativo');
+                    }
+                    return false;   
+                }
+            });
+        });
+    }
+    
+    await attachEditHandler();
+    return editButton;
+}
+
+async function checkRevision(targa, dataRev) {
+    return new Promise((resolve, reject) => {
+        handleAjaxRequest(
+            '../php/search_targa.php',
+            'GET',
+            "targa=" + targa,
+            function(response) {
+                if (response.success === true) {
+                    const dataEm = response.data[0]['dataEm'];
+                    const dataRes = response.data[0]['dataRes'];
+                    const dataEmObj = new Date(dataEm);
+                    const dataResObj = new Date(dataRes);
+                    const dataRevObj = new Date(dataRev);
+                    
+                    if (dataRevObj < dataEmObj) {
+                        alert("Data di revisione antecedente alla data di emissione della targa associata");
+                        resolve(false);
+                    } else if (dataRevObj > dataResObj) {
+                        alert("Data di revisione posteriore alla data di restituzione della targa associata");
+                        resolve(false);
+                    } else {
+                        resolve(true);
+                    }
+                } else {
+                    alert("Targa non presente nel database");
+                    resolve(false);
+                }
+            },
+            function(xhr, status, error) {
+                console.error('Error', xhr.responseText);
+                alert("Error occurred while fetching data.");
+                resolve(false);
+            }
+        );
+    });
+}
+
+async function saveChanges(dataUpdateRequest) {
+    return new Promise((resolve, reject) => {
+        handleAjaxRequest(
+            '../php/search_revisione.php',
+            'POST',
+            dataUpdateRequest,
+            function(response) {
+                handleResponse(response, "Istanza modificata correttamente");
+                resolve(response);
+            },
+            function(xhr, status, error) {
+                handleAjaxError(xhr.responseText);
+                resolve(error);
+            }
+        );
+    });
+}
+
+function handleResponse(response, successMessage) {
+    console.log('Response:', response.message);
+    if (response.success === true) {
+        alert(successMessage);
+        //performDefaultSearch();
+    } else {
+        alert("Operazione non riuscita");
+    }
+}
+
+function handleAjaxError(responseText) {
+    console.error('Error', responseText);
+    alert("Error occurred while fetching data.");
 }
--- src/js/searchRevisione.js
+++ src/js/searchRevisione.js
@@ -1,11 +1,8 @@
 $(document).ready(function() {
     $('#searchForm').submit(searchFormSubmitted);
     $('#addForm').submit(addFormSubmitted);
-    $('#editForm').submit(editFormSubmitted);
     $(document).on('click', '.deleteBtn', deleteBtnClicked);
-    $(document).on('click', '.editBtn', editBtnClicked);
     $('#addEsito').change(addEsitoChanged); // need to be copied for the #editEsitoS
-    $(document).on('click', '.addButton', addButtonClicked);
 
     performDefaultSearch();
 
@@ -15,50 +12,14 @@ $(document).ready(function() {
         performSearch(formData);
     }
 
-    function addFormSubmitted(event) {
+    async function addFormSubmitted(event) {
         event.preventDefault();
         var formData = $(this).serialize() + '&action=create';
         var targa = $('#addTarga').val();
         var dataRev = $('#addDataRev').val();
-        checkRevisionAndPerformAction(formData, targa, dataRev, performAddAction);
-    }
-
-    function editFormSubmitted(event) {
-        event.preventDefault();
-        var formData = $(this).serialize() + '&action=update';
-        var targa = $('#editTarga').val();
-        var dataRev = $('#editDataRev').val();
-        checkRevisionAndPerformAction(formData, targa, dataRev, performEditAction);
-    }
-
-    function checkRevisionAndPerformAction(formData, targa, dataRev, actionCallback) {
-        handleAjaxRequest(
-            '../php/search_targa.php',
-            'GET',
-            "targa=" + targa,
-            function(response) {
-                if (response.success === true) {
-                    var dataEm = response.data[0]['dataEm'];
-                    var dataRes = response.data[0]['dataRes'];
-                    var dataEmObj = new Date(dataEm);
-                    var dataResObj = new Date(dataRes);
-                    var dataRevObj = new Date(dataRev);
-                    if (dataRevObj < dataEmObj) {
-                        alert("Data di revisione antecedente alla data di emissione della targa associata");
-                    } else if (dataRevObj > dataResObj) {
-                        alert("Data di revisione posteriore alla data di restituzione della targa associata");
-                    } else {
-                        actionCallback(formData);
-                    }
-                } else {
-                    alert("Targa non presente nel database");
-                }
-            },
-            function(xhr, status, error) {
-                console.error('Error', xhr.responseText);
-                alert("Error occurred while fetching data.");
-            }
-        );
+        if (await checkRevision(targa, dataRev)) {
+            performAddAction(formData);
+        }
     }
 
     function performAddAction(formData) {
@@ -68,20 +29,6 @@ $(document).ready(function() {
             formData,
             function(response) {
                 handleResponse(response, "Istanza inserita correttamente");
-            },
-            function(xhr, status, error) {
-                handleAjaxError(xhr.responseText);
-            }
-        );
-    }
-
-    function performEditAction(formData) {
-        handleAjaxRequest(
-            '../php/search_revisione.php',
-            'POST',
-            formData,
-            function(response) {
-                handleResponse(response, "Istanza modificata correttamente");
             },
             function(xhr, status, error) {
                 handleAjaxError(xhr.responseText);
@@ -99,50 +46,9 @@ $(document).ready(function() {
         }
     }
 
-    function deleteBtnClicked() {
-        var id = $(this).closest('li').data('id');
-        var confirmed = confirm("Are you sure you want to delete this entry?");
-        if (confirmed) {
-            handleAjaxRequest(
-                '../php/search_revisione.php',
-                'POST',
-                { action: 'delete', id: id},
-                function(response) {
-                    handleResponse(response, "Elemento rimosso");
-                },
-                function(xhr, status, error) {
-                    handleAjaxError(xhr.responseText);
-                }
-            );
-        }
-    }
-
-    function editBtnClicked() {
-        var listItem = $(this);
-        var id = listItem.find('.numero');
-        console.log(id);
-        var data = listItem.text().split('fatta il ')[1].split(' alla targa ')[0];
-        var targa = listItem.text().split('targa ')[1].split(',')[0];
-        var esito = listItem.text().includes('esito positivo') ? 'positivo' : 'negativo';
-        var motivazione = esito === 'negativo' ? listItem.text().split('La motivazione: ')[1].split('EditDelete')[0] : '';
-        $('#editId').val(id);
-        $('#editDataRev').val(data);
-        $('#editTarga').val(targa);
-        $('#editEsito').val(esito);
-        $('#editMotivazione').val(motivazione);
-        $('#editMotivazioneDiv').toggle(esito === 'negativo');
-        $('.addForm').hide();
-        $('.editForm').show();            
-    }
-
     function addEsitoChanged() {
         $('#addMotivazioneDiv').toggle($(this).val() === 'negativo');
         $('#addMotivazione').prop('required', $(this).val() === 'negativo');
-    }
-
-    function addButtonClicked() {
-        $('.addForm').show();
-        $('.editForm').hide(); 
     }
 
     function performDefaultSearch() {
